d714ddac913596e4100cd6450a33cb55d419b348
Alan Maginn <p.alan.maginn@gmail.com>
2023-11-25 17:40:55 -0500

    make fibonacci sequence work

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let newNum = parseInt(n);
+    if (newNum < 0 ) {
+        return "OOPS"
+    } else if (newNum <= 1) {
+        return newNum;
+    } 
+    return fibonacci(newNum -1) + fibonacci(newNum -2);
 };
 
 // Do not edit below this line
