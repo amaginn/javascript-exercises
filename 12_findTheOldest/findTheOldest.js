75484078ddfc2e97f8c45e3e1024bc6715073d8d
Alan Maginn <p.alan.maginn@gmail.com>
2023-11-28 08:33:45 -0500

    make findOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people.filter (person => {
+        if (person.hasOwnProperty('yearOfDeath') === false && (new Date().getFullYear() - person.yearOfBirth) > 100) {
+            return true
+        } 
+        else if ((person.yearOfDeath - person.yearOfBirth) > 48) { 
+            return true
+        }
+    });
+    return oldest[0];
+   
+    
 
+    // const oldest = people.sort(function(a, b){
+    //     const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    //     const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    //     lastGuy < nextGuy ? -1 : 1;
+    //   })
+    //   console.table(oldest);
 };
 
 // Do not edit below this line
